cea4b350ed5832197168bb795313c6f3c2f264a9
joelias marcos de andrade <joeliasmarcos.andrade@outlook.com>
2020-10-07 22:46:03 -0300

    Atualizado 2 - Inclusão de Migration

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -14,5 +14,12 @@ namespace projetoIBM.Data
 
         public DbSet<Prestacao> Prestacaos { get; set; }
         public DbSet<Contrato> Contratos  { get; set; }
+
+        //  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       /// {
+          //  optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Vendas;Data Source=DESKTOP-S8GP951\\sqlexpress");
+
+
+    //    }
     }
 }
